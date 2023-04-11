275bdad58c57bda67757c7df5c8d808b5abf9de4
Shailesh Shenoy <shenoy.sh@northeastern.edu>
2023-04-11 15:39:51 -0400

    Refactored findAll() method using criteriaBuilder() Added spring security to the application

--- src/main/java/com/info6250/eventpawz/model/user/AppUserDao.java
+++ src/main/java/com/info6250/eventpawz/model/user/AppUserDao.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 @Repository
@@ -23,11 +25,30 @@ public class AppUserDao {
     }
 
     public List<AppUser> findAll() {
-        return sessionFactory.getCurrentSession().createQuery("from AppUser", AppUser.class).list();
+        CriteriaQuery<AppUser> criteriaQuery = sessionFactory.getCurrentSession().getCriteriaBuilder().createQuery(AppUser.class);
+        Root<AppUser> root = criteriaQuery.from(AppUser.class);
+        criteriaQuery.select(root);
+
+        return sessionFactory.getCurrentSession().createQuery(criteriaQuery).getResultList();
     }
 
     public AppUser findByUsername(String username) {
         return sessionFactory.getCurrentSession().get(AppUser.class, username);
     }
+
+    public AppUser findByEmail(String email) {
+        return sessionFactory.getCurrentSession().get(AppUser.class, email);
+    }
+
+    //Update user
+    public void update(AppUser appUser) {
+        sessionFactory.getCurrentSession().update(appUser);
+    }
+
+    //Delete user
+    public void delete(AppUser appUser) {
+        sessionFactory.getCurrentSession().delete(appUser);
+    }
+
 
 }
