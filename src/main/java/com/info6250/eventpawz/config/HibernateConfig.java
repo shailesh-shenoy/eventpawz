ed69ba3ab78cc23c1044734d62bfbe410efe2664
Shailesh Shenoy <shenoy.sh@northeastern.edu>
2023-04-11 01:24:48 -0400

    Added lombok getter to hibernate config to fix Config processing

--- src/main/java/com/info6250/eventpawz/config/HibernateConfig.java
+++ src/main/java/com/info6250/eventpawz/config/HibernateConfig.java
@@ -1,5 +1,6 @@
 package com.info6250.eventpawz.config;
 
+import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.cfg.Environment;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import javax.sql.DataSource;
 import java.util.Properties;
 
+@Getter
 @Setter
 @Configuration
 @EnableTransactionManagement
